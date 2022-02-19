8065d06c0aabe02180fc48044b2ffb791bd57a1e
barrownicholas <me@nicholasrbarrow.com>
2022-02-18 23:00:19 -0500

    0.0.1

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-package-YOUR-USERNAME-HERE",
+    name="PyMacApp",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="The Nicholas R. Barrow Company, LLC",
+    author_email="me@nicholasrbarrow.com",
+    description="A boilerplate for creating MacOS desktop apps and installers, including signing and notorizing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    package_dir={"": "boilerplate"},
+    packages=setuptools.find_packages(where="boilerplate"),
+    python_requires=">=3.9",
+    install_requires=["PyInstaller"]
 )
